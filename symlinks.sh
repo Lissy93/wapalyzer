b7d6799ea1fd9a7644ab345e039f6b4bb31df4c4
ElbertF <info@elbertf.com>
2011-12-26 13:29:12 +1100

    Added symlinks script

---
+++ symlinks.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+ln -is ../../../../share/images/icons     adapters/firefox/skin/images/
+ln -is ../../../../share/js/lib           adapters/firefox/content/js/
+ln -is ../../../../share/js/apps.js       adapters/firefox/content/js/
+ln -is ../../../../share/js/wappalyzer.js adapters/firefox/content/js/
+
+ln -is    ../../../share/images/icons     adapters/html/images/
+ln -is    ../../../share/js/lib           adapters/html/js/
+ln -is    ../../../share/js/apps.js       adapters/html/js/
+ln -is    ../../../share/js/wappalyzer.js adapters/html/js/
+
