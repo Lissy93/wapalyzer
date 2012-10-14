bf22c298525bdeeb5c3f8db7d7097f5b750072e6
Elbert Alias <elbert@alias.io>
2012-10-14 16:42:36 +1100

    Work on Jetpack driver

--- drivers/firefox-jetpack/lib/driver.js
+++ drivers/firefox-jetpack/lib/driver.js
@@ -1,4 +1,28 @@
-var wappalyzer = {};
+/**
+ * Jetpack driver
+ */
 
-require('wappalyzer');
-require('apps');
+(function() {
+	//'use strict';
+	var
+		main = require('wappalyzer'),
+		w    = main.wappalyzer
+		;
+
+	w.driver = {
+		/**
+		 * Log messages to console
+		 */
+		log: function(args) {
+			console.log(args.message);
+		},
+
+		/**
+		 * Initialize
+		 */
+		init: function(callback) {
+		}
+	}
+
+	w.init();
+})();
