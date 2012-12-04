0bbe7c69a5ecb11ced2269f98df3e3ef1fe12d2b
q-- <anonymous-contributor@example.com>
2012-12-04 16:42:01 +0100

    Prompt the user to manually download 7-Zip

--- package.cmd
+++ package.cmd
@@ -8,12 +8,29 @@
 ::	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 @echo off
 
+::Detect HTA host
+if exist %windir%\SysWOW64\mshta.exe goto hta64
+if exist %windir%\System32\mshta.exe goto hta32
+:hta32
+set hta=%windir%\System32\mshta.exe 
+:hta64
+set hta=%windir%\SysWOW64\mshta.exe 
+goto endDetection
+:endDetection
+
 ::Links
 call links.cmd
 
 ::Remove old packages
 if exist wappalyzer-chrome.zip del wappalyzer-chrome.zip
 if exist wappalyzer-firefox.xpi del wappalyzer-firefox.xpi
+
+
+::Prompt to download 7-Zip if necessary
+set dl7zfile=.windows_zip\Download 7-Zip.hta
+:retry
+if not exist .windows_zip\7za.exe cd .windows_zip&%hta% "%cd%\%dl7zfile%"&cd ..&goto retry
+
 
 ::Pack Chrome extension
 cd drivers\chrome&&..\..\.windows_zip\7za.exe a -tzip -mx9 ..\..\wappalyzer-chrome.zip *
@@ -24,5 +41,5 @@ cd drivers\firefox&&..\..\.windows_zip\7za.exe a -tzip -mx9 ..\..\wappalyzer-fir
 cd..\..
 
 
-
+:end
 @echo on
