45bd501fcb3f4a45232a6486e6f24e82eb93fb7f
Elbert Alias <elbert@alias.io>
2017-12-08 07:56:40 +1100

    Updated styled-components icon

--- src/drivers/webextension/js/iframe.js
+++ src/drivers/webextension/js/iframe.js
@@ -181,7 +181,7 @@ var exports = {};
 				video_assets: opt_video_assets,
 				assets: opt_assets,
 				version: '3',
-				mrev: 'b084b53-c',
+				mrev: '4aeaa5a-c',
 				msgNum: this.msgNum,
 				timestamp: new Date().getTime(),
 				pageVis: document.visibilityState,
@@ -890,7 +890,7 @@ var exports = {};
 	var _pageTags;
 	var INIT_MS_BW_SEARCHES = 2000;
 	var PAGE_TAG_RE = new RegExp('gpt|oascentral');
-	var POST_MSG_ID = '1510070508-5767-8848-29475-8984';
+	var POST_MSG_ID = '1511804838-25881-9878-26947-14879';
 	var AD_SERVER_RE = new RegExp('^(google_ads_iframe|oas_frame|atwAdFrame)');
 
 	function getPageTags(doc) {
