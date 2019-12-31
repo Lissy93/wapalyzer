635f6871d4edc3f53c4d4677841d3adbadcf2d20
Elbert Alias <77259+AliasIO@users.noreply.github.com>
2019-12-31 15:57:06 +1100

    Refactor Puppeteer browser for NPM driver

--- src/drivers/npm/browsers/puppeteer.js
+++ src/drivers/npm/browsers/puppeteer.js
@@ -1,198 +1,125 @@
-const { Cluster } = require('puppeteer-cluster');
+const puppeteer = require('puppeteer');
 const Browser = require('../browser');
 
-let cluster = null;
-
-function puppeteerJsEvalFunction() {
-  const shallowMap = (origin, level) => {
+function getJs() {
+  const dereference = (obj, level = 0) => {
     try {
-      if (level === 0) {
-        return true;
+      if (level > 5 || typeof obj === 'function') {
+        return undefined;
       }
 
-      if (typeof origin === 'string' || typeof origin === 'number') {
-        return origin;
+      if (Array.isArray(obj)) {
+        obj = obj.map(item => dereference(item, level + 1));
       }
 
-      if (typeof origin === 'undefined' || origin === null) {
-        return false;
+      if (typeof obj === 'object' && obj !== null) {
+        const newObj = {};
+
+        Object.keys(obj).forEach((key) => {
+          newObj[key] = dereference(obj[key], level + 1);
+        });
+
+        return newObj;
       }
 
-      if ((typeof origin === 'object' || typeof origin === 'function') && origin.hasOwnProperty) {
-        const ret = {};
-        Object.keys(origin)
-          .forEach((key) => {
-            ret[key] = shallowMap(origin[key], level - 1);
-          });
-        return ret;
-      }
-
-      return true;
-    } catch (err) {
-      return false;
+      return obj;
+    } catch (error) {
+      return undefined;
     }
   };
 
-  // prevent cross-origin error
-  window.frames = [];
-
   // eslint-disable-next-line no-undef
-  return shallowMap(window, 6);
+  return dereference(window);
 }
-
 
 class PuppeteerBrowser extends Browser {
   constructor(options) {
-    super();
-    this.options = Object.assign(
-      {},
-      {
-        puppeteerClusterOptions: {
-          concurrency: Cluster.CONCURRENCY_CONTEXT,
-          maxConcurrency: 4,
-          puppeteerOptions: {
-            headless: false,
-            ignoreHTTPSErrors: true,
-          },
-        },
-      },
-      options,
-    );
-    this.resources = [];
+    options.maxWait = options.maxWait || 60;
 
-    this.links = [];
+    super(options);
 
-    this.window = {};
-    this.cookies = [];
-    this.scripts = [];
-    this.page = null;
-    this.js = {};
-  }
-
-  async visit(visiturl) {
-    let visitcb = null;
-    const newPromise = new Promise((resolve, reject) => {
-      visitcb = (err) => {
-        if (err) {
-          return reject(err);
-        }
-        return resolve();
-      };
+    this.browser = () => puppeteer.launch({
+      args: ['--no-sandbox', '--headless', '--disable-gpu'],
     });
-    // start cluster
-    if (!cluster) {
-      cluster = await Cluster.launch(this.puppeteerClusterOptions);
-      this.log('Cluster started', 'puppeteer');
-      await cluster.task(async ({ page, data: { url, cb, myContext } }) => {
-        await myContext.visitInternal(page, url, cb);
-      });
-    }
-
-    await cluster.queue({ url: visiturl, cb: visitcb, myContext: this });
-    return newPromise;
   }
 
-  async visitInternal(page, url, cb) {
-    this.log(`Opening: ${url}`, 'puppeteer');
+  async visit(url) {
+    const browser = await this.browser();
 
-    this.resources = [];
-    this.links = [];
-    this.scripts = [];
-    this.headers = [];
-    this.window = {};
+    const page = await browser.newPage();
 
-    try {
-      await page.setRequestInterception(true);
+    await page.setRequestInterception(true);
 
-      this.page = page;
+    page.on('request', request => request.continue());
 
-      page.on('request', (req) => {
-        req.continue();
-      });
-
-      page.on('response', (res) => {
-        if (res.status() === 301 || res.status() === 302) {
-          return;
-        }
-        const headers = res.headers();
-
-        if (this.resources.length === 0) {
-          this.statusCode = res.status();
-          this.contentType = headers['content-type'];
-          Object.keys(headers).forEach((key) => {
-            if (Array.isArray(headers[key])) {
-              this.headers[key] = headers[key];
-            } else {
-              this.headers[key] = [headers[key]];
-            }
-          });
-        }
-
-        this.resources.push(res.url());
-
-        if (
-          headers['content-type']
-                    && (headers['content-type'].indexOf('javascript') !== -1
-                        || headers['content-type'].indexOf('application/') !== -1)
-        ) {
-          this.scripts.push(res.url());
-        }
-      });
-
-      // navigate
-      await page.setUserAgent(this.options.userAgent);
-      try {
-        if (this.options.waitDuration) {
-          await Promise.race([
-            page.goto(url, {
-              timeout: this.options.waitDuration,
-              waitUntil: 'networkidle2',
-            }),
-            new Promise(x => setTimeout(x, this.options.waitDuration)),
-          ]);
-        } else {
-          await page.goto(url, {
-            waitUntil: 'networkidle2',
-          });
-        }
-      } catch (err) {
-        this.log(err.toString(), 'puppeteer', 'error');
+    page.on('response', (response) => {
+      if (response.status() === 301 || response.status() === 302) {
+        return;
       }
 
-      // get links
+      if (!this.statusCode) {
+        this.statusCode = response.status();
+
+        this.headers = {};
+
+        const headers = response.headers();
+
+        Object.keys(headers).forEach((key) => {
+          this.headers[key] = Array.isArray(headers[key]) ? headers[key] : [headers[key]];
+        });
+
+        this.contentType = headers['content-type'] || null;
+      }
+    });
+
+    await page.setUserAgent(this.options.userAgent);
+
+    try {
+      await Promise.race([
+        page.goto(url, {
+          timeout: this.options.maxWait,
+          waitUntil: 'networkidle2',
+        }),
+        new Promise(resolve => setTimeout(resolve, this.options.maxWait)),
+      ]);
+
       // eslint-disable-next-line no-undef
-      const list = await page.evaluateHandle(() => Array.from(document.getElementsByTagName('a')).map(a => ({
-        href: a.href,
-        hostname: a.hostname,
-        pathname: a.pathname,
-        hash: a.hash,
-        protocol: a.protocol,
+      const links = await page.evaluateHandle(() => Array.from(document.getElementsByTagName('a')).map(({
+        hash, hostname, href, pathname, protocol, rel,
+      }) => ({
+        hash,
+        hostname,
+        href,
+        pathname,
+        protocol,
+        rel,
       })));
-      this.links = await list.jsonValue();
 
-      // a very simple representation of the window object
-      this.js = await this.page.evaluate(puppeteerJsEvalFunction);
+      this.links = await links.jsonValue();
 
-      // get cookies
-      this.cookies = await page.cookies();
-      this.cookies = this.cookies.map((e) => {
-        e.key = e.name;
-        return e;
-      });
+      // eslint-disable-next-line no-undef
+      const scripts = await page.evaluateHandle(() => Array.from(document.getElementsByTagName('script')).map(({
+        src,
+      }) => src));
 
-      // get html
+      this.scripts = (await scripts.jsonValue()).filter(script => script);
+
+      this.js = await page.evaluate(getJs);
+
+      this.cookies = (await page.cookies()).map(({
+        name, value, domain, path,
+      }) => ({
+        name, value, domain, path,
+      }));
+
       this.html = await page.content();
-
-      // close the page to free up memory
-      await page.close();
-      this.page = null;
-
-      // close everything
-      cb();
-    } catch (err) {
-      this.log(err.toString(), 'puppeteer', 'error');
-      cb(err);
+    } catch (error) {
+      throw new Error(error.message);
     }
+
+    await page.close();
+
+    this.page = null;
   }
 }
 
