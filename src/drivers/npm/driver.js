190cb3d34735d97378f9777b1f5f34ce9f3e9815
Elbert Alias <77259+AliasIO@users.noreply.github.com>
2020-01-04 13:39:41 +1100

    Remove uncaughtException listener

--- src/drivers/npm/driver.js
+++ src/drivers/npm/driver.js
@@ -109,8 +109,6 @@ class Driver {
     this.wappalyzer.driver.log = (message, source, type) => this.log(message, source, type);
     this.wappalyzer.driver
       .displayApps = (detected, meta, context) => this.displayApps(detected, meta, context);
-
-    process.on('uncaughtException', e => this.wappalyzer.log(`Uncaught exception: ${e.message}`, 'driver', 'error'));
   }
 
   on(event, callback) {
