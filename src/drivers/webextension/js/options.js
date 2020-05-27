cb1668fb042729ab65a4e04b501e5069e294ed0c
Elbert Alias <77259+AliasIO@users.noreply.github.com>
2020-05-28 08:57:53 +1000

    Add alerts, fix dark mode

--- src/drivers/webextension/js/options.js
+++ src/drivers/webextension/js/options.js
@@ -3,29 +3,29 @@
 /* globals browser Wappalyzer */
 /* eslint-env browser */
 
-const wappalyzer = new Wappalyzer();
+const wappalyzer = new Wappalyzer()
 
 /**
  * Get a value from localStorage
  */
 function getOption(name, defaultValue = null) {
   return new Promise(async (resolve, reject) => {
-    let value = defaultValue;
+    let value = defaultValue
 
     try {
-      const option = await browser.storage.local.get(name);
+      const option = await browser.storage.local.get(name)
 
       if (option[name] !== undefined) {
-        value = option[name];
+        value = option[name]
       }
     } catch (error) {
-      wappalyzer.log(error.message, 'driver', 'error');
+      wappalyzer.log(error.message, 'driver', 'error')
 
-      return reject(error.message);
+      return reject(error.message)
     }
 
-    return resolve(value);
-  });
+    return resolve(value)
+  })
 }
 
 /**
@@ -34,72 +34,76 @@ function getOption(name, defaultValue = null) {
 function setOption(name, value) {
   return new Promise(async (resolve, reject) => {
     try {
-      await browser.storage.local.set({ [name]: value });
+      await browser.storage.local.set({ [name]: value })
     } catch (error) {
-      wappalyzer.log(error.message, 'driver', 'error');
+      wappalyzer.log(error.message, 'driver', 'error')
 
-      return reject(error.message);
+      return reject(error.message)
     }
 
-    return resolve();
-  });
+    return resolve()
+  })
 }
 
 document.addEventListener('DOMContentLoaded', async () => {
-  const nodes = document.querySelectorAll('[data-i18n]');
+  const nodes = document.querySelectorAll('[data-i18n]')
 
   Array.prototype.forEach.call(nodes, (node) => {
-    node.childNodes[0].nodeValue = browser.i18n.getMessage(node.dataset.i18n);
-  });
+    node.childNodes[0].nodeValue = browser.i18n.getMessage(node.dataset.i18n)
+  })
 
   document.querySelector('#github').addEventListener('click', () => {
-    window.open(wappalyzer.config.githubURL);
-  });
+    window.open(wappalyzer.config.githubURL)
+  })
 
   document.querySelector('#twitter').addEventListener('click', () => {
-    window.open(wappalyzer.config.twitterURL);
-  });
+    window.open(wappalyzer.config.twitterURL)
+  })
 
   document.querySelector('#wappalyzer').addEventListener('click', () => {
-    window.open(wappalyzer.config.websiteURL);
-  });
+    window.open(wappalyzer.config.websiteURL)
+  })
 
-  let el;
-  let value;
+  let el
+  let value
 
   // Upgrade message
-  value = await getOption('upgradeMessage', true);
+  value = await getOption('upgradeMessage', true)
 
-  el = document.querySelector('#option-upgrade-message');
+  el = document.querySelector('#option-upgrade-message')
 
-  el.checked = value;
+  el.checked = value
 
-  el.addEventListener('change', e => setOption('upgradeMessage', e.target.checked));
+  el.addEventListener('change', (e) =>
+    setOption('upgradeMessage', e.target.checked)
+  )
 
   // Dynamic icon
-  value = await getOption('dynamicIcon', true);
+  value = await getOption('dynamicIcon', true)
 
-  el = document.querySelector('#option-dynamic-icon');
+  el = document.querySelector('#option-dynamic-icon')
 
-  el.checked = value;
+  el.checked = value
 
-  el.addEventListener('change', e => setOption('dynamicIcon', e.target.checked));
+  el.addEventListener('change', (e) =>
+    setOption('dynamicIcon', e.target.checked)
+  )
 
   // Tracking
-  value = await getOption('tracking', true);
+  value = await getOption('tracking', true)
 
-  el = document.querySelector('#option-tracking');
+  el = document.querySelector('#option-tracking')
 
-  el.checked = value;
+  el.checked = value
 
-  el.addEventListener('change', e => setOption('tracking', e.target.checked));
+  el.addEventListener('change', (e) => setOption('tracking', e.target.checked))
 
   // Theme Mode
-  value = await getOption('themeMode', false);
+  value = await getOption('themeMode', false)
 
-  el = document.querySelector('#option-theme-mode');
+  el = document.querySelector('#option-theme-mode')
 
-  el.checked = value;
+  el.checked = value
 
-  el.addEventListener('change', e => setOption('themeMode', e.target.checked));
-});
+  el.addEventListener('change', (e) => setOption('themeMode', e.target.checked))
+})
