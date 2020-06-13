1924be9d149b115f00483a11a33ee32496c51b58
Elbert Alias <77259+AliasIO@users.noreply.github.com>
2020-06-14 09:13:30 +1000

    Fix link script

--- bin/link.js
+++ bin/link.js
@@ -1,18 +1,17 @@
 const fs = require('fs')
 
-// WebExtension
-fs.unlinkSync('./src/drivers/webextension/apps.json')
-fs.linkSync('./src/apps.json', './src/drivers/webextension/apps.json')
+const link = (src, dest) => {
+  if (fs.statSync(dest).isFile()) {
+    fs.unlinkSync(dest)
+  }
 
-fs.unlinkSync('./src/drivers/webextension/js/wappalyzer.js')
-fs.linkSync(
-  './src/wappalyzer.js',
-  './src/drivers/webextension/js/wappalyzer.js'
-)
+  fs.linkSync(src, dest)
+}
+
+// WebExtension
+link('./src/apps.json', './src/drivers/webextension/apps.json')
+link('./src/wappalyzer.js', './src/drivers/webextension/js/wappalyzer.js')
 
 // NPM
-fs.unlinkSync('./src/drivers/npm/apps.json')
-fs.linkSync('./src/apps.json', './src/drivers/npm/apps.json')
-
-fs.unlinkSync('./src/drivers/npm/wappalyzer.js')
-fs.linkSync('./src/wappalyzer.js', './src/drivers/npm/wappalyzer.js')
+link('./src/apps.json', './src/drivers/npm/apps.json')
+link('./src/wappalyzer.js', './src/drivers/npm/wappalyzer.js')
