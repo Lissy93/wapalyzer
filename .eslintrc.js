cd68f007fe16281a4adb337cdf0dc7bb83c36874
Johannes Andersen <johannes.andersen@signifyd.com>
2020-11-15 16:54:49 +0000

    Add linting for JSON and workflow to test on push

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,19 +2,18 @@ module.exports = {
   root: true,
   env: {
     browser: true,
-    node: true
+    node: true,
   },
   parserOptions: {
-    parser: 'babel-eslint'
+    parser: 'babel-eslint',
   },
   extends: [
     '@nuxtjs',
     'prettier',
     'prettier/vue',
     'plugin:prettier/recommended',
-    'plugin:nuxt/recommended'
+    'plugin:nuxt/recommended',
+    'plugin:json/recommended',
   ],
-  plugins: [
-    'prettier'
-  ],
+  plugins: ['prettier'],
 }
