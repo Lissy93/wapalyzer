0d60e6225295044c847bf9b31600573179c39ed6
Elbert Alias <77259+AliasIO@users.noreply.github.com>
2022-10-15 14:56:40 +1100

    Build v6.10.42

--- src/drivers/webextension/js/driver.js
+++ src/drivers/webextension/js/driver.js
@@ -119,10 +119,10 @@ const Driver = {
         'https://www.wappalyzer.com/installed/?utm_source=installed&utm_medium=extension&utm_campaign=wappalyzer'
       )
     } else if (version !== previous && upgradeMessage) {
-      open(
-        `https://www.wappalyzer.com/upgraded/?utm_source=upgraded&utm_medium=extension&utm_campaign=wappalyzer`,
-        false
-      )
+      // open(
+      //   `https://www.wappalyzer.com/upgraded/?utm_source=upgraded&utm_medium=extension&utm_campaign=wappalyzer`,
+      //   false
+      // )
     }
 
     await setOption('version', version)
